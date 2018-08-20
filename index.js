d5c71ac053e23e58e7702adc0d2b3361b6a2408b
Kuntal <kuntalkantidas.besu@gmail.com>
2018-08-20 22:21:36 +0530

    Heroku Update

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ var fs = require("fs");
 // var jsonData = require("./temp.json"); 
 var path = require("path");
 
-app.get('/list', function (req, res) 
-{ 
+app.set('port',(process.env.PORT || 8082));
+
+app.get('/', function (req, res) { 
+    res.send("Welcome to heroku !!!");
+}) 
+
+app.get('/list', function (req, res) { 
     fs.readFile( __dirname + "/" + "temp.json", 'utf8', function (err, data) { 
         console.log( data ); 
         res.end( data ); 
     }); 
 }) 
 
-app.post('/addtemp/:temp', function (req, res) { 
+app.get('/addtemp/:temp', function (req, res) { 
     data = req.params.temp; 
     fs.appendFile( __dirname + "/" + "temp.json", "\n"+data , function(err) {
         if(err){throw err}
-        else{console.log("")}
+        else{res.send(data)}
     }) 
 }) 
 
-var server = app.listen(8082, function () { 
+var server = app.listen(app.get('port'), function () {
     var host = server.address().address 
     var port = server.address().port 
     console.log("Example app listening at http://%s:%s", host, port) 
